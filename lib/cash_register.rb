eb78ace27fb8cd219bb02c6d6ccf39d434f6fe95
Denali Balser <denali.balser167@gmail.com>
2019-08-27 16:40:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,12 +2,12 @@ class CashRegister
   attr_accessor :discount
 
   def initialize(discount = 0)
-    @total = 0
+    @@total = 0
     @discount = discount
   end
 
   def total
-    @total 
+    @@total 
   end
 
 
