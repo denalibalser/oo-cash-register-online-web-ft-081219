0c0036bed86542055b77ae5ed36ff5f4238025f9
Denali Balser <denali.balser167@gmail.com>
2019-08-27 16:38:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,13 @@
 class CashRegister
+  attr_accessor :employee_discount
 
-  def initialize(employee_discount = nil)
+  def initialize(employee_discount = 0)
     @total = 0
     @employee_discount = employee_discount
   end
+
+  def total
+
 
 
 
